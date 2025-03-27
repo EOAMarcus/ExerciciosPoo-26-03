af44941ef4b858a0a1bfff99a8c70f40ae5f12e3
Marcus Eugenio Oliveira de Araujo <eoa.marcus@gmail.com>
2025-03-26 21:49:37 -0300

    Add files via upload

---
+++ Ex2/ContaBancária.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_2
+{
+    class ContaBancaria
+    {
+        public string NConta;
+        public string DonoConta;
+        public double Saldo;
+
+        public void Depositar(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public void Sacar(double valor)
+        {
+            if (Saldo >= valor)
+            {
+                Saldo -= valor;
+            }
+            else
+            {
+                Console.WriteLine("Sem Saldo.");
+            }
+        }
+        public void ExibirSaldo()
+        {
+            Console.WriteLine($"Saldo agora: {Saldo}");
+        }
+    }
+
+
+}
